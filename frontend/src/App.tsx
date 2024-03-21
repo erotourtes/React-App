c77c391c1e1747223165a809c25ae6f3db815166
erotourtes <erotourtes@gmail.com>
2024-03-21 22:46:12 +0200

    Added basic outlook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,93 @@
+import { Plus, History, EllipsisVertical, Dot, Calendar } from "lucide-react";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Badge } from "./components/ui/badge";
+import { Card, CardContent, CardHeader } from "./components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { H3 } from "./components/ui/typography";
+
+function NavBar() {
+  return (
+    <nav className="flex justify-between items-center">
+      <H3>My Task Board</H3>
+      <div className="space-x-5">
+        <Button variant="outline" className="gap-2">
+          <History />
+          History
+        </Button>
+        <Button className="gap-2">
+          <Plus />
+          Create new list
+        </Button>
+      </div>
+    </nav>
+  );
+}
+
+function TaskList() {
+  return (
+    <div className="w-[250px]">
+      <div className="flex justify-between border-t-2 border-b-2 py-2 px-1 border-secondary">
+        <p>To Do</p>
+        <div className="flex gap-4">
+          <p>45</p>
+          <EllipsisVertical />
+        </div>
+      </div>
+      <Button variant="outline" className="w-full gap-2">
+        <Plus />
+        Add new card
+      </Button>
+      <TaskCard />
+    </div>
+  );
+}
+
+function TaskCard() {
+  return (
+    <Card>
+      <CardHeader className="p-3 flex-row justify-between">
+        Task 1
+        <EllipsisVertical />
+      </CardHeader>
+      <CardContent className="p-3 flex flex-col">
+        <p className="text-[0.9rem] opacity-grayish">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className="flex gap-3">
+          <Calendar />
+          <span className="opacity-grayish">Wed, 19 Apr</span>
+        </div>
+        <div>
+          <Badge className="px-3 py-2">Badge</Badge>
+        </div>
+        <Select>
+          <SelectTrigger className="w-full bg-secondary">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </CardContent>
+    </Card>
+  );
+}
 
 function App() {
   return (
     <>
-      <Button>Click me</Button>
+      <NavBar />
+      <TaskList />
     </>
   );
 }
