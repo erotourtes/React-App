64ff588cd3e17c20ecd88f2c1cc78c2eac0730fd
erotourtes <erotourtes@gmail.com>
2024-03-23 10:50:19 +0200

    Added history sheet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Plus, History, EllipsisVertical, Dot, Calendar } from "lucide-react";
+import { Plus, History, EllipsisVertical, Calendar } from "lucide-react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Badge } from "./components/ui/badge";
@@ -11,16 +11,42 @@ import {
   SelectValue,
 } from "./components/ui/select";
 import { H3 } from "./components/ui/typography";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./components/ui/sheet";
 
 function NavBar() {
   return (
     <nav className="flex justify-between items-center">
       <H3>My Task Board</H3>
       <div className="space-x-5">
-        <Button variant="outline" className="gap-2">
-          <History />
-          History
-        </Button>
+        <Sheet>
+          <SheetTrigger>
+            <Button variant="outline" className="gap-2">
+              <History />
+              History
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <SheetHeader>
+              <SheetTitle className="bg-secondary fg-secondary-foreground px-3 py-4 text-left">
+                History
+              </SheetTitle>
+              <SheetDescription className="p-3 text-left">
+                <ul>
+                  <li>1</li>
+                  <li>1</li>
+                </ul>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
         <Button className="gap-2">
           <Plus />
           Create new list
