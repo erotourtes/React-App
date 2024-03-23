c5b1c49bd438c74ba3759b7c9a7c1ce22a720369
erotourtes <erotourtes@gmail.com>
2024-03-23 12:05:18 +0200

    Added task menu for task list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,7 +77,25 @@ function TaskList() {
         <p>To Do</p>
         <div className="flex gap-4">
           <p>45</p>
-          <EllipsisVertical />
+          <DropdownMenu>
+            <DropdownMenuTrigger className="hover:bg-accent hover:text-accent-foreground rounded-sm">
+              <EllipsisVertical />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <PopupIcon icon={<Pencil />} />
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <PopupIcon icon={<Plus />} />
+                Add new card
+              </DropdownMenuItem>
+              <DropdownMenuItem className="des-btn focus:des-btn-rev">
+                <PopupIcon icon={<Trash2 />} />
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
       <Button variant="outline" className="w-full gap-2 border-dashed border-2">
