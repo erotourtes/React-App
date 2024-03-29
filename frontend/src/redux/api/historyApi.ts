c0dd6ac85f0bbfdaf8c53e2d91a326bc274802e4
erotourtes <erotourtes@gmail.com>
2024-03-29 10:14:48 +0200

    Refactor(ws): extracted to a service

--- frontend/src/redux/api/historyApi.ts
+++ frontend/src/redux/api/historyApi.ts
@@ -1,72 +1,33 @@
 import { api } from "@/redux/api/apiSlice";
+import { WebSocketService } from "@/redux/api/wsService";
 import { HistoryT } from "@shared/dtos";
+
+const wsService = WebSocketService.getInstace();
 
 export const historyApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getAllHistory: builder.query<HistoryT[], void>({
       query: () => `history/tasks`,
-      onCacheEntryAdded: async (
-        arg,
-        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
-      ) => {
-        const ws = new WebSocket("ws://localhost:3000");
-        try {
-          await cacheDataLoaded;
-
-          const listener = (event: MessageEvent) => {
-            const data = JSON.parse(event.data) as {
-              event: string;
-              data: HistoryT;
-            };
-            if (data.event !== "history:task:new") return;
-            updateCachedData((draft) => {
-              draft?.push(data.data);
-            });
-          };
-
-          ws.addEventListener("message", listener);
-        } catch (error) {
-          console.error("Error in websocket connection", error);
-        }
-
-        await cacheEntryRemoved;
-        ws.close();
+      onCacheEntryAdded: async (arg, { updateCachedData }) => {
+        wsService.on<HistoryT>("history:task:new", (data) => {
+          updateCachedData((draft) => {
+            draft?.push(data);
+          });
+        });
       },
     }),
     getHistoryForTask: builder.query<HistoryT[], number>({
       query: (taskId) => `history/tasks/${taskId}`,
-      onCacheEntryAdded: async (
-        arg,
-        { cacheDataLoaded, cacheEntryRemoved, dispatch }
-      ) => {
-        const ws = new WebSocket("ws://localhost:3000");
-        try {
-          await cacheDataLoaded;
-
-          const listener = (event: MessageEvent) => {
-            const data = JSON.parse(event.data) as {
-              event: string;
-              data: HistoryT;
-            };
-            if (data.event !== "history:task:new") return;
-            console.log("New task history", data.data);
-
-            dispatch(
-              historyApi.util.updateQueryData(
-                "getHistoryForTask",
-                +data.data.recordId,
-                (tasks) => tasks.concat(data.data)
-              )
-            );
-          };
-
-          ws.addEventListener("message", listener);
-        } catch (error) {
-          console.error("Error in websocket connection", error);
-        }
-
-        await cacheEntryRemoved;
-        ws.close();
+      onCacheEntryAdded: async (arg, { dispatch }) => {
+        wsService.on<HistoryT>("history:task:new", (data) => {
+          dispatch(
+            historyApi.util.updateQueryData(
+              "getHistoryForTask",
+              +data.recordId,
+              (tasks) => tasks.concat(data)
+            )
+          );
+        });
       },
     }),
   }),
